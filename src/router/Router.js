781fb2569257fc41c811666189731b3195074899
kwakhyun <khyun2348@naver.com>
2022-09-17 11:33:42 +0900

    Feat: Route 추가

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Main } from "../pages/Main";
+import { Login } from "../pages/Login";
+import { Join } from "../pages/Join";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/join" element={<Join />} />
     </Routes>
   );
 };
