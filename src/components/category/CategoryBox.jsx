46871a06330c7919caf48a213d6c6bc663f8eb4a
LEE-YO-HAN <yhl0078@gmail.com>
2022-09-17 18:57:35 +0900

    add: category Image 추가

--- src/components/category/CategoryBox.jsx
+++ src/components/category/CategoryBox.jsx
@@ -1,5 +1,25 @@
-
+import styled from "styled-components"
 
 export const CategoryBox =()=>{
     
+    return(
+        <>
+            
+            <StyledCategoryImg src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F0Ei09%2FbtrMkt3aikH%2FKHuEMXcruHUwZF5PiU0zC1%2Fimg.png" alt="https://icons8.com/icon/2169/multiple-devices Multiple Devices</a> icon by https://icons8.com" />
+            <StyledCategoryImg src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FxantZ%2FbtrMkG2snSo%2FGSsMszSJxprlT1H3onWaK1%2Fimg.png" alt="https://icons8.com/icon/ARGuqYp5gd84/toolbox Toolbox</a> icon by https://icons8.com" />
+            <StyledCategoryImg src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbY2ql0%2FbtrMjWxXjed%2Fmki0iUHV5bQ1TvK3cwsFh1%2Fimg.png" alt="https://icons8.com/icon/xLqAXsDsOHIe/appliances Appliances</a> icon by https://icons8.com" />
+            <StyledCategoryImg src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbPqfgC%2FbtrMkH1ni36%2FJNSYgpfFKFpBdt04Q60Xhk%2Fimg.png" alt="https://icons8.com/icon/tz3NPpBJTT76/bag Bag</a> icon by https://icons8.com" />
+            <StyledCategoryImg src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FchJ0gv%2FbtrMkHmKs27%2FD5BqKMJ0r8H5WtgPkHUQkK%2Fimg.png" alt="https://icons8.com/icon/AMKxEjBJw38y/sports Sports</a> icon https://icons8.com" />
+            <StyledCategoryImg src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcCRpGL%2FbtrMjuV2cls%2FxJP26hbWKrYeulNe40VxZK%2Fimg.png" alt="https://icons8.com/icon/JtRYpjtdhzhs/game-controller Game Controller icon by https://icons8.com" />
+            <StyledCategoryImg src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcClWfP%2FbtrMnmvejE7%2Fox4bf2buG60CkelvwQxEV0%2Fimg.png" alt="https://icons8.com/icon/nt7jrhMPqVjW/storytelling Storytelling icon by https://icons8.com" />
+            <StyledCategoryImg src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FImLIT%2FbtrMjEdhqOt%2FyzIK3cunHdoswF8CdKVeBK%2Fimg.png" alt="https://icons8.com/icon/5344/medium-priority Medium Priority icon by https://icons8.com" />
+        </>
+    )
 }
+
+
+const StyledCategoryImg = styled.img`
+    width:50px;
+    height:50px;
+    margin-left:50px;
+`
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 
+
 export const Header = () => {
   return (
     <StyledHeaderTop>
-      <h1></h1>
+      <div>
+      <StyledLogoImg src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcIaEmP%2FbtrMnmILkqF%2FTxtrB71zUMzMY9Z2iiHqcK%2Fimg.png" />
+      </div>
     </StyledHeaderTop>
   );
 };
 
 const StyledHeaderTop = styled.header`
-  position: fixed;
+  position: absolute;
   top: 0px;
   left: 0px;
+  
   width: 100%;
-  background-color: black;
+  height:78px;
 
-  color:white;
+
+  
 `;
+
+const StyledLogoImg = styled.img`
+  width:300px;
+  height:80px;
+`
