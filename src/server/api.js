97aba34a0dfda848afeb2982b503c58030733d2c
kwakhyun <khyun2348@naver.com>
2022-09-20 21:08:11 +0900

    Add: axios 객체, api 추가

--- src/server/api.js
+++ src/server/api.js
@@ -1,13 +1,14 @@
+import { base } from "./core/instance";
 import { auth } from "./core/instance";
 
 export const productAPI = {
-  getProducts: auth.get(`/products`),
+  getProducts: base.get(`/products`),
+  getCategory: (categoryId) => base.get(`/category${categoryId}`),
+  getProductDetail: (productId) => base.get(`/products/${productId}`),
+
   addProduct: (data) => auth.post(`/products`, data),
-  getProductDetail: (productId) => auth.get(`/products/${productId}`),
   updateProduct: (data) => auth.put(`/products/${data.productId}`, data),
   deleteProduct: (productId) => auth.delete(`/products/${productId}`),
 };
-
-export const categoryAPI = {};
 
 export const userAPI = {};
