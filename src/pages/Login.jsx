8aec154fb5b458778905e872bf8b9a92f2723ae2
kwakhyun <khyun2348@naver.com>
2022-09-27 20:51:29 +0900

    Fix: 로그인, 회원가입 요청 url 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ export const Login = () => {
   const password = useRef(null);
 
   const handleLogin = async (data) => {
-    return await axios.post("http://52.79.235.129/logins", {
+    return await axios.post("https://davidpai.shop/logins", {
       email: data.email,
       password: data.password,
     });
