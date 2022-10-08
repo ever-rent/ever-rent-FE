df5e9a9f34e1455610e9d146171e4cde1cacb93d
kwakhyun <khyun2348@naver.com>
2022-10-08 09:06:00 +0900

    Feat: 토큰 만료 상태 관리 추가, api 코드 정리

--- src/server/core/instance.js
+++ src/server/core/instance.js
@@ -2,26 +2,17 @@ import axios from "axios";
 
 export const base = axios.create({
   baseURL: "http://13.209.8.18",
-  // baseURL: "http://3.35.19.62:8080", // ㅎㅈ님
-  // baseURL: "http://davidpai.shop",
 });
 
 export const auth = axios.create({
-  baseURL: "http://13.209.8.18/auth",
-  // baseURL: "http://3.35.19.62:8080/auth",
-  // baseURL: "http://davidpai.shop/auth",
-});
-
-export const profile = axios.create({
   baseURL: "http://13.209.8.18",
-  // baseURL: "http://3.35.19.62:8080", // ㅎㅈ님
-  // baseURL: "https://davidpai.shop",
+  // baseURL: "http://3.35.19.62:8080",
+  // baseURL: "http://davidpai.shop",
 });
 
 auth.interceptors.request.use((config) => {
   const accessToken = localStorage.getItem("accessToken");
   const refreshToken = localStorage.getItem("refreshToken");
-
   if (!accessToken || !refreshToken) {
     config.headers["authorization"] = null;
     config.headers["refresh-token"] = null;
@@ -29,21 +20,16 @@ auth.interceptors.request.use((config) => {
     config.headers["authorization"] = accessToken;
     config.headers["refresh-token"] = refreshToken;
   }
-
   return config;
 });
 
-profile.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem("accessToken");
-  const refreshToken = localStorage.getItem("refreshToken");
-
-  if (!accessToken || !refreshToken) {
-    config.headers["authorization"] = null;
-    config.headers["refresh-token"] = null;
-  } else {
-    config.headers["authorization"] = accessToken;
-    config.headers["refresh-token"] = refreshToken;
+auth.interceptors.response.use((response) => {
+  if (response.headers["authorization"]) {
+    localStorage.removeItem("accessToken");
+    localStorage.setItem("accessToken", response.headers["authorization"]);
+  } else if(response.data.error === "INVALID_TOKEN") {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    window.location.href = "/login";
   }
-
-  return config;
 });
