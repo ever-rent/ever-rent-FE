23c1de0e6071823c77999a904a7be3c7e597ad92
jeongwon <rladnjs15@naver.com>
2022-10-13 01:46:45 +0900

    Feat: dateToTime 수정중

--- src/components/main/Banner.jsx
+++ src/components/main/Banner.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import { Desktop, Mobile } from "../../Hooks/MideaQuery";
+import DesktopBanner from "../../image/bannerImg.png";
+import MobileBanner from "../../image/bannerMobileImg.png";
 
 export const Banner = () => {
   return (
     <>
       <Desktop>
-        <StyledBanner src="img/bannerImg.png" alt="이미지 중비중 입니다." />
+        <StyledBanner src={DesktopBanner} alt="이미지 중비중 입니다." />
       </Desktop>
 
       <Mobile>
-        <StyledMobileBanner
-          src="img/bannerMobileImg.png"
-          alt="이미지 중비중 입니다."
-        />
+        <StyledMobileBanner src={MobileBanner} alt="이미지 중비중 입니다." />
       </Mobile>
     </>
   );
