47af16ac282f4db7d54e72ad7548ee762cef7122
LEE-YO-HAN <yhl0078@gmail.com>
2022-10-14 19:48:55 +0900

    update: kakao map search in addProduct/editProduct.jsx imageModal.jsx delete console

--- src/components/imageModal/ImageModal.jsx
+++ src/components/imageModal/ImageModal.jsx
@@ -10,32 +10,33 @@ export const ImageModal = ({
   imageIndex,
   closeImage,
 }) => {
-  console.log(imageIndex);
-  console.log(imageArray);
 
+  // 가짜 이미지를 포함한 전체 배열 길이
+  // 좌우로 이동할 길이
   let idxIndex = imageArray?.length + 2;
   let idxRight = 620;
 
+  // 현재 이미지 number
+  // 현재 idx(x축 위치) 
+  // transition 시간 초기화
   const [imageNum, setImageNum] = useState(imageIndex);
   const [idxPx, setIdxPx] = useState(0);
   const [idxInit, setIdxInit] = useState(0.25);
 
-  console.log("idx픽셀", idxPx);
-  console.log("idx라이트", idxRight);
-
+  // 상세페이지 호출시 첫 번째 사진 위치로 이동
   useEffect(() => {
     setTimeout(() => {
       setIdxPx(idxPx - idxRight);
     }, 200);
   }, []);
 
+  // 클릭한 subImage 위치로 이동
   const imageChange = (index) => {
     setImageNum(index);
-
-    setIdxPx(-idxRight * (imageNum + 1));
+    setIdxPx(-idxRight * (index + 1));
   };
-  console.log(imageNum);
 
+  // 첫 번째 사진이 아니면 이전 사진으로 이동
   const goPrev = (e) => {
     e.stopPropagation();
     if (imageNum !== 0) {
@@ -44,6 +45,8 @@ export const ImageModal = ({
     } else {
       setImageNum(imageArray.length - 1);
       setIdxPx(idxPx + idxRight);
+      // 가짜 이미지로 이동하는 척 하면서
+      // transition 시간을 0으로 바꾸고 기존 이미지 배열로 이동
       setTimeout(() => {
         setIdxInit(0);
         setIdxPx(imageArray.length * -idxRight);
@@ -51,6 +54,8 @@ export const ImageModal = ({
     }
     setIdxInit(0.25);
   };
+
+  // 마지막 사진이 아니면 처음 사진으로 이동
   const goNext = (e) => {
     e.stopPropagation();
     if (imageNum !== imageArray.length - 1) {
@@ -59,6 +64,8 @@ export const ImageModal = ({
     } else {
       setImageNum(0);
       setIdxPx(idxPx - idxRight);
+      // 가짜 이미지로 이동하는 척 하면서
+      // transition 시간을 0으로 바꾸고 기존 이미지 배열로 이동
       setTimeout(() => {
         setIdxInit(0);
         setIdxPx(idxRight * -1);
