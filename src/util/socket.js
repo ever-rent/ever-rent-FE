9ae070631d5a4033d6fe191fc45f6ab83ece287c
kwakhyun <khyun2348@naver.com>
2022-10-24 13:32:50 +0900

    Remove: unused files

--- src/util/socket.js
+++
@@ -1,7 +0,0 @@
-import SockJS from "sockjs-client";
-import StompJS from "stompjs";
-
-export const stompConnect = (token, userId) => {
-  const socket = new SockJS("http://");
-  const stompClient = StompJS.over(socket);
-};
