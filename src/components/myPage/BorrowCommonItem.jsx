32076405f6b1bdc742d4cb80e72ae6957ba50fc6
jeongwon <rladnjs15@naver.com>
2022-10-25 21:52:47 +0900

    Feat: 빌린물건 모바일 수정, 목록 시간 수정

--- src/components/myPage/BorrowCommonItem.jsx
+++ src/components/myPage/BorrowCommonItem.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Desktop, Mobile } from "../../Hooks/MideaQuery";
+import { dateToTime } from "../../util/timeToToday";
 
 export const BorrowCommonItem = ({ item, index }) => {
-  const { imgUrl, productName, content } = item;
-  
+  const { imgUrl, productName, buyStart, buyEnd } = item;
   //TODO: 아직 정해지지 않았지만 일단 빌린물건 목록에만 img가 나오도록 설정.
   const insertImg = (index) => {
     if (index === 0) {
@@ -13,25 +14,58 @@ export const BorrowCommonItem = ({ item, index }) => {
     }
   };
 
+  //남은 기간
+  const [rentStatus, setRentStatus] = useState("");
+
+  useEffect(() => {
+    let timeStatus = item?.buyEnd;
+    setRentStatus(dateToTime(new Date(timeStatus)));
+  }, [buyEnd, item]);
+
   return (
-    <StyledItem>
-      {/* <img src={imgUrl} alt="img" /> */}
-      {insertImg(index)}
-      <div className="span-div">
-        <span className="title">{productName}</span>
-        <span>{content}</span>
-        <span>
-          남은 기간 : <span className="date">3일</span>
-        </span>
-      </div>
-    </StyledItem>
-  )
-}
+    <>
+      <Desktop>
+        <StyledItem>
+          <StyledContentBox className="span-div">
+            <span className="title">{productName}</span>
+            <span className="apply">
+              <div>
+                신청 기간 : {buyStart} ~ {buyEnd}
+              </div>
+            </span>
+            <span className="date">
+              <div>마감기한 : {rentStatus}</div>
+            </span>
+          </StyledContentBox>
+        </StyledItem>
+      </Desktop>
+
+      <Mobile>
+        <StyledMobileItem>
+          <div className="span-div">
+            <span className="title">{productName}</span>
+            <span className="apply">
+              <div>
+                신청 기간 : {buyStart} ~ {buyEnd}
+              </div>
+            </span>
+            <span className="date">
+              <div>마감기한 : {rentStatus}</div>
+            </span>
+          </div>
+        </StyledMobileItem>
+      </Mobile>
+    </>
+  );
+};
 
 const StyledItem = styled.div`
+  /* border: 1px solid red; */
+  width: 600px;
   display: flex;
   position: relative;
   padding: 10px;
+  margin-bottom: 5px;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   .like {
     position: absolute;
@@ -49,12 +83,97 @@ const StyledItem = styled.div`
     display: flex;
     flex-direction: column;
     .title {
-      color: #999;
-      font-size: 12px;
+      font-size: 17px;
+      font-weight: 600;
+      /* margin-bottom: 5px; */
     }
     .date {
+      margin-top: 5px;
+      font-size: 15px;
       color: red;
-      font-weight: bold;
+      font-weight: 500;
+    }
+    .content {
+      color: #999;
+    }
+    .price {
+      /* color: #0092f3; */
+      font-weight: 600;
+      /* font-size: 12px; */
+    }
+    .period {
+      color: #999;
+      font-size: 13px;
+    }
+    .day {
+      font-size: 13px;
+    }
+    .resevation {
+      font-size: 15px;
+      margin-top: 9px;
+    }
+    .apply {
+      color: #999;
+      font-size: 13px;
+      margin-top: 9px;
+    }
+  }
+`;
+
+const StyledContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const StyledMobileItem = styled.div`
+  /* border: 1px solid red; */
+  display: flex;
+  position: relative;
+  padding: 10px;
+  /* margin-bottom: 7px; */
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  .like {
+    position: absolute;
+    top: 5px;
+    right: 10px;
+    font-size: 30px;
+    cursor: pointer;
+  }
+  img {
+    width: 100px;
+    height: 100px;
+    margin-right: 10px;
+  }
+  .span-div {
+    display: flex;
+    flex-direction: column;
+    .title {
+      color: black;
+      font-size: 17px;
+      font-weight: 600;
+      margin-bottom: 5px;
+    }
+    .date {
+      font-size: 15px;
+      color: red;
+      font-weight: 500;
+      margin-top: 5px;
+    }
+    .content {
+      color: #999;
+    }
+    .price {
+      /* color: #0092f3; */
+      font-weight: 600;
+      /* font-size: 12px; */
+    }
+    .period {
+      color: #999;
+      font-size: 13px;
+    }
+    .day {
+      font-size: 13px;
     }
   }
 `;
