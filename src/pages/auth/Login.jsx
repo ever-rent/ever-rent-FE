cf26810d425b7347eaf939242a0d7faa0dba512c
kwakhyun <khyun2348@naver.com>
2022-12-08 23:56:35 +0900

    Refactor: Login page refactoring

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -4,9 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { useMutation } from "react-query";
 import axios from "axios";
 import { StyledLogin } from "./styled";
-import { Toast } from "../../util/toast";
-import styled from "styled-components";
-import { FcGoogle } from "react-icons/fc";
+import { handleToast, Toast } from "../../util/toast";
+// import { FcGoogle } from "react-icons/fc";
 
 export const Login = () => {
   const email = useRef(null);
@@ -28,63 +27,50 @@ export const Login = () => {
         localStorage.setItem("memberName", data.data.data.memberName);
         localStorage.setItem("accessToken", data.headers["authorization"]);
         localStorage.setItem("refreshToken", data.headers["refresh-token"]);
-        Toast.fire({
-          icon: "success",
-          title: "로그인 성공! 환영합니다!",
-        });
+        handleToast("success", "로그인 성공! 환영합니다!");
         navigate("/");
       }
     },
     onError: ({ response }) => {
       if (response.status === 400) {
-        Toast.fire({
-          icon: "error",
-          title:
-            "일치하는 계정이 없습니다. 이메일 또는 비밀번호를 확인해주세요.",
-        });
-      } else {
-        Toast.fire({
-          icon: "error",
-          title: "로그인에 실패했습니다. 다시 시도해주세요.",
-        });
+        handleToast(
+          "error",
+          "일치하는 계정이 없습니다. 이메일 또는 비밀번호를 확인해주세요."
+        );
+        return;
       }
+      handleToast("error", "로그인에 실패했습니다. 다시 시도해주세요.");
     },
   });
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (email.current.value === "") {
+      handleToast("error", "이메일을 입력해주세요.");
+      email.current.focus();
+      return;
+    }
+    if (password.current.value === "") {
+      handleToast("error", "비밀번호를 입력해주세요.");
+      password.current.focus();
+      return;
+    }
+    mutate({
+      email: email.current.value,
+      password: password.current.value,
+    });
+  };
+
   return (
     <StyledLogin>
-      <img src={logo} alt="logo" />
-      <label>이메일</label>
-      <input type="email" ref={email} placeholder="이메일 입력" />
-      <label>비밀번호</label>
-      <input type="password" ref={password} placeholder="비밀번호 입력" />
-      <button
-        onClick={() => {
-          if (email.current.value === "") {
-            Toast.fire({
-              icon: "error",
-              title: "이메일을 입력해주세요.",
-            });
-            email.current.focus();
-            return;
-          }
-          if (password.current.value === "") {
-            Toast.fire({
-              icon: "error",
-              title: "비밀번호를 입력해주세요.",
-            });
-            password.current.focus();
-            return;
-          }
-          mutate({
-            email: email.current.value,
-            password: password.current.value,
-          });
-        }}
-      >
-        로그인
-      </button>
-      {/* <StyledGoogleLogin
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <img src={logo} alt="logo" />
+        <label>이메일</label>
+        <input type="email" ref={email} placeholder="이메일 입력" />
+        <label>비밀번호</label>
+        <input type="password" ref={password} placeholder="비밀번호 입력" />
+        <button type="submit">로그인</button>
+        {/* <StyledGoogleLogin
         onClick={() => {
           window.location.href = `${process.env.REACT_APP_GOOGLE_ACCOUNT}`;
         }}
@@ -96,6 +82,7 @@ export const Login = () => {
           <span>Google 계정으로 로그인</span>
         </div>
       </StyledGoogleLogin> */}
+      </form>
       <div className="span-box">
         <span onClick={() => navigate("/forgotPw")}>비밀번호 찾기</span>
         <span onClick={() => navigate("/join")}>회원가입</span>
@@ -104,28 +91,28 @@ export const Login = () => {
   );
 };
 
-const StyledGoogleLogin = styled.span`
-  border: none;
-  padding: 0px;
-  margin-top: 15px;
-  border-radius: 20px;
-  font-size: 15px;
-  font-weight: bold;
-  width: 300px;
-  height: 40px;
-  color: #000;
-  background-color: #fff;
-  z-index: 3;
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  .icon-box {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    .google-icon {
-      margin: 5px 5px 0 0;
-    }
-  }
-`;
+// const StyledGoogleLogin = styled.span`
+//   border: none;
+//   padding: 0px;
+//   margin-top: 15px;
+//   border-radius: 20px;
+//   font-size: 15px;
+//   font-weight: bold;
+//   width: 300px;
+//   height: 40px;
+//   color: #000;
+//   background-color: #fff;
+//   z-index: 3;
+//   a {
+//     text-decoration: none;
+//     color: inherit;
+//   }
+//   .icon-box {
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     .google-icon {
+//       margin: 5px 5px 0 0;
+//     }
+//   }
+// `;
